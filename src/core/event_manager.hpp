53ac7c8c78eba5949f08b5cb8a1ae92fc9ae4274
DarkoiV <makkob997@gmail.com>
2022-02-12 22:06:04 +0100

    Stylistic change

--- src/core/event_manager.hpp
+++ src/core/event_manager.hpp
@@ -7,7 +7,7 @@
 
 class EventManager
 {
-    // Event HandlerFn is function that takes SDL_Event
+    // HandlerFn is function that takes SDL_Event ref
     // and returns True when event was processed
     using HandlerFn = std::function<bool(SDL_Event&)>;
 
@@ -18,13 +18,7 @@ class EventManager
     {
         HandlerFn function;
         int priority = 0;
-
-        Handler()
-        {
-            ID = nextID;
-            nextID++;
-        }
-        
+ 
         Uint32 id() const
         {
            return ID;
@@ -32,7 +26,7 @@ class EventManager
 
     private:
         static inline Uint32 nextID = 0;
-        Uint32 ID;
+        Uint32 ID = nextID++;
     };
 
     std::vector<Handler> m_handlers;
