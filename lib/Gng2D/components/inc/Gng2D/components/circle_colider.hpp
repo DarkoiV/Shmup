201039279f2e201865fed42e3a586461f4ddf789
Maksymilian <makkob997@gmail.com>
2023-02-02 16:40:05 +0100

    Added collision system stub

---
+++ lib/Gng2D/components/inc/Gng2D/components/circle_colider.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Gng2D
+{
+struct CircleCollider
+{
+    float radius;
+};
+}
+
---
+++ lib/Gng2D/systems/inc/Gng2D/systems/collision_system.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include <type_traits>
+#include <cmath>
+#include <functional>
+#include "entt/entity/registry.hpp"
+
+namespace Gng2D
+{
+struct CircleCollider;
+struct Position;
+
+template<typename C>
+concept CircleColliderType = std::is_base_of<CircleCollider, C>::value;
+
+template<CircleColliderType A, CircleColliderType B>
+struct CollisionSystem
+{
+    using CollisionHandler = std::function<void(entt::entity, entt::entity)>;
+
+    CollisionSystem(entt::registry& r, CollisionHandler ch)
+        : registry(r)
+        , collisionHandler(ch)
+    {};
+
+    void operator()()
+    {
+        auto colliderAView = registry.view<Position, A>();
+        auto colliderBView = registry.view<Position, B>();
+        for (const auto& [enttityA, posA, colliderA] : colliderAView.each())
+        {
+            for (const auto& [enttityB, posB, colliderB] : colliderBView.each())
+            {
+                auto distance = std::sqrt(std::pow(posA.x - posB.x) + std::pow(posA.y - posB.y));
+                if (distance < (colliderA.radius + colliderB.radius))
+                {
+                    collisionHandler(enttityA, enttityB);
+                }
+            }
+        }
+    }
+
+private:
+    entt::registry&     registry;
+    CollisionHandler    collisionHandler;
+};
+}
