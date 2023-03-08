813f8fa455e62d95c41111f10c56b12b82f33b84
Maksymilian <makkob997@gmail.com>
2023-03-08 17:35:56 +0100

    Smol changes

--- source/src/systems/player_weapons.cpp
+++ source/src/systems/player_weapons.cpp
@@ -6,8 +6,8 @@ void PlayerWeapons::primaryFire()
     if (not remainingCooldown)
     {
         const auto& playerPosition = playerShip.getComponent<Gng2D::Position>();
-        const Gng2D::Position bulletSpawnPosition{playerPosition.x, playerPosition.y - 10};
-        scene.spawnObject<AllyBullet>(bulletSpawnPosition, Gng2D::Velocity{0, -5});
+        const Gng2D::Position bulletSpawnPosition{playerPosition.x, playerPosition.y - 15};
+        scene.spawnObject<AllyBullet>(bulletSpawnPosition, Gng2D::Velocity{0, -4});
         remainingCooldown = cooldownFrames;
     }
 }
