3845de207750b4550b7e76507d1c08b958675773
Maksymilian <makkob997@gmail.com>
2023-05-25 19:29:06 +0200

    Added animation system

---
+++ components/inc/Gng2D/components/animation.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "Gng2D/types/coroutine.hpp"
+
+namespace Gng2D
+{
+struct Animation : Coroutine
+{
+    using Gng2D::Coroutine::Coroutine;
+};
+}
+
---
+++ systems/src/animation_system.cpp
@@ -0,0 +1,23 @@
+#include "Gng2D/systems/animaton_system.hpp"
+#include <algorithm>
+#include <execution>
+
+using Gng2D::AnimationSystem;
+
+AnimationSystem::AnimationSystem(entt::registry& r)
+    : reg(r)
+{
+}
+
+void AnimationSystem::operator()()
+{
+    auto view = reg.view<Animation>();
+    std::for_each(std::execution::par_unseq, view.begin(), view.end(), 
+    [&](auto entity)
+    {
+        auto&& [animation] = view.get(entity);
+        animation();
+        if (animation.isCompleted()) reg.erase<Animation>(entity);
+    });
+}
+
